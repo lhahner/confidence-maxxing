82a51e949a2aca8ac333bb78e338aa2e4dff823c
lhahner <lennart.hahner@gmail.com>
2026-08-23 09:45:03 +0200

    adds detections for transfusion and yolo for nuscenes mini, provide first outline/draft of algorithm

--- main.py
+++ main.py
@@ -1,3 +1,46 @@
+from pyquaternion import Quaternion
+from nuscenes.utils.data_classes import Box
+
+def detector_box_lidar_to_camera(
+    box,
+    lidar_to_global,
+    cam_pose,
+    cam_cs,
+):
+    T = np.asarray(lidar_to_global)
+    R_lidar_global = T[:3, :3]
+    t_lidar_global = T[:3, 3]
+    box.rotate(Quaternion(matrix=R_lidar_global))
+    box.translate(t_lidar_global)
+    box.translate(
+        -np.asarray(cam_pose["translation"])
+    )
+    box.rotate(
+        Quaternion(cam_pose["rotation"]).inverse
+    )
+    box.translate(
+        -np.asarray(cam_cs["translation"])
+    )
+    box.rotate(
+        Quaternion(cam_cs["rotation"]).inverse
+    )
+    return box
+
+def detection_to_box(det):
+    x, y, z, yaw, length, width, height, score = det["bbox_3d"]
+
+    return Box(
+        center=[x, y, z],
+        size=[width, length, height],  # nuScenes expects w, l, h
+        orientation=Quaternion(
+            axis=[0, 0, 1],
+            radians=yaw,
+        ),
+        score=score,
+        name=det["label"],
+    )
+
+
 def parse_args():
     pass
 
@@ -5,7 +48,18 @@ def load_3D_detections(path):
     pass
 
 def transform_3D_to_2D():
-    pass
+    x, y, z, yaw, length, width, height, score = det["bbox_3d"]
+     Box(
+        center=[x, y, z],
+        size=[width, length, height],  # nuScenes expects w, l, h
+        orientation=Quaternion(
+            axis=[0, 0, 1],
+            radians=yaw,
+        ),
+        score=score,
+        name=det["label"],
+    )
+
 
 def load_2D_detections(path):
     pass
@@ -23,12 +77,16 @@ def main():
     args = parse_args()
 
     detections_3D = load_3D_detections(path=args.3d_detection_path)
-    detections_3D_in_2D = transform_3D_to_2D(detections_3D=detections_3D)
-
-    detections_2D = load_2D_detections(path=args.2d_detection_path)
-
-    assoication_map = asscoiate(detections_3D_in_2D, detections_2D)
-    maxxed_detections = maxx(association_map==assoication_map)
+    for detection_3D in detections_3D:
+        detection_3D_in_2D = transform_3D_to_2D(detections_3D=detection)
+        
+        detections_2D = load_2D_detections(path=args.2d_detection_path, sample_token="")
+        for detection_2D in detections_2D:
+            center_based_distance = compute_center_based_distance(detection_3D_in_2D, detection_2D)
+            if center_based_distance > 1.5:
+                continue 
+            if detection_3D_in_2D["confidence"] < 0.2:
+                detection_3D_in_2D["confidence"] += detection_2D["confidence"]
 
     with open(OUTPUT_FILE, "w") as file:
         io.write(maxxed_detections, file)
